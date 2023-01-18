ab4da473f5e84ded754993e4295c1fb5c447396b
= <=>
2023-01-18 19:22:03 +0300

    committed

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,64 +1,80 @@
 #!/usr/bin/env python3
 """
- a Cache class
- generate a random key (e.g. using uuid),
- store the input data in Redis using the random key and return the key.
+Redis basic.
 """
-from typing import Callable, Optional, Union
+from typing import Union, Callable, Optional
 from functools import wraps
 import redis
-import sys
-from uuid import uuid4
+import uuid
 
 
-def count_calls(func: Callable) -> Callable:
-    """ a decorator to count how many times a function is called """
-    key: str = func.__qualname__
+def call_history(method: Callable) -> Callable:
+    """Stores the history of inputs and outputs for a particular function"""
+    method_key = method.__qualname__
+    inputs, outputs = method_key + ':inputs', method_key + ':outputs'
 
-    @wraps(func)
+    @wraps(method)
     def wrapper(self, *args, **kwargs):
-        """ a function to be wrapped """
-        self._redis.incr(key, amount=1)
-        return func(self, *args, **kwargs)
+        self._redis.rpush(inputs, str(args))
+        result = method(self, *args, **kwargs)
+        self._redis.rpush(outputs, str(result))
+        return result
     return wrapper
 
 
-class Cache:
-    """store an instance of the Redis client as a private variable named"""
+def count_calls(method: Callable) -> Callable:
+    """Creates and returns function that increments the count \
+        for that key every time the method is called and returns \
+        the value returned by the original method"""
+    method_key = method.__qualname__
 
-    def __init__(self) -> None:
-        """instantiate the class"""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        self._redis.incr(method_key)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+
+def replay(method: Callable) -> None:
+    """Displays the history of calls of a particular function"""
+    method_key = method.__qualname__
+    inputs, outputs = method_key + ':inputs', method_key + ':outputs'
+    redis = method.__self__._redis
+    method_count = redis.get(method_key).decode('utf-8')
+    print(f'{method_key} was called {method_count} times:')
+    IOTuple = zip(redis.lrange(inputs, 0, -1), redis.lrange(outputs, 0, -1))
+    for inp, outp in list(IOTuple):
+        attr, data = inp.decode("utf-8"), outp.decode("utf-8")
+        print(f'{method_key}(*{attr}) -> {data}')
+
+
+class Cache:
+    """Cache class to handle redis operations."""
+    def __init__(self):
+        """stores an instance of the Redis client."""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def get_str(self, key: str) -> str:
-        """ converts byte to str """
-        data = self._redis.get(key)
-        return data.decode('UTF-8')
-
-    def get_int(self, key: str) -> int:
-        data = self._redis.get(key)
-        try:
-            data = int(data.decode('UTF-8'))
-        except Exception:
-            data = 0
-        return data
-
+    @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """
-        store the input data in Redis using the random key and return the key
-        """
-        key: str = str(uuid4())
-        self._redis.set(key, data)
+        """Takes and stores a data argument and returns a string."""
+        key = str(uuid.uuid4())
+        self._redis.mset({key: data})
         return key
 
-    def get(self, key: str,
-            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
-        """ Gets  """
-        res = self._redis.get(key)
-        try:
-            res = fn(res) if fn else res
-        except ValueError as err:
-            pass
-        return res
+    def get(self,
+            key: str, fn: Optional[Callable] = None) -> str:
+        """Takes a key string argument and an optional.
+        Callable argument named fn. This callable will be used to\
+            convertthe data back to a desired format."""
+        data = self._redis.get(key)
+        return fn(data) if fn is not None else data
+
+    def get_str(self, data: str) -> str:
+        """Returns str value of decoded byte """
+        return data.decode('utf-8', 'strict')
+
+    def get_int(self, data: str) -> int:
+        """Returns int value of decoded byte """
+        return int(data)
